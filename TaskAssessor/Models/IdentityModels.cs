45461448ee7e0dd526defa9d790f3efe052301b0
pcorneliu <corneliu@rubrikkgroup.com>
2017-12-06 15:47:01 +0200

    Added JobModel

--- TaskAssessor/Models/IdentityModels.cs
+++ TaskAssessor/Models/IdentityModels.cs
@@ -20,6 +20,8 @@ namespace TaskAssessor.Models
 
     public class ApplicationDbContext : IdentityDbContext<ApplicationUser>
     {
+        public DbSet<Job> Jobs { get; set; }
+
         public ApplicationDbContext()
             : base("DefaultConnection", throwIfV1Schema: false)
         {
---
+++ TaskAssessor/Models/Job.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace TaskAssessor.Models
+{
+    public class Job
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public DateTime DateAdded { get; set; }
+        public DateTime DateModified { get; set; }
+        public string AddedBy { get; set; }
+    }
+}
