7badd6c4938d5186082985fe0775d3dc546cc627
pcorneliu <corneliu@rubrikkgroup.com>
2017-12-06 15:23:51 +0200

    Add project files.

---
+++ TaskAssessor/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(TaskAssessor.Startup))]
+namespace TaskAssessor
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
