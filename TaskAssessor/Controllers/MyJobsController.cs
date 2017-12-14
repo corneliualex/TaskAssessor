7dc36cbb383be06a8a35ba7ced0c579245531376
pcorneliu <corneliu@rubrikkgroup.com>
2017-12-14 18:47:40 +0200

    Added validations and changed the functionalities a bit for MyJobs

--- TaskAssessor/Controllers/MyJobsController.cs
+++ TaskAssessor/Controllers/MyJobsController.cs
@@ -16,13 +16,14 @@ namespace TaskAssessor.Controllers
     public class MyJobsController : Controller
     {
         private ApplicationDbContext _context = new ApplicationDbContext();
-
+ 
         public ActionResult Index(string date)
         {
             if (date == null)
                 date = SetDate(DateTime.Now);
 
-            var currentUserJobs = _context.HourIntervals.Include(u => u.Job).ToList().Where(u => u.ApplicationUserId == User.Identity.GetUserId() && date.Equals(SetDate(u.DateAdded)));
+            var currentUserId = GetCurrentUserId();
+            var currentUserJobs = _context.HourIntervals.Include(u => u.Job).ToList().Where(u => u.ApplicationUserId == currentUserId && date.Equals(SetDate(u.DateAdded)));
             if (currentUserJobs == null)
                 return View();
 
@@ -46,12 +47,13 @@ namespace TaskAssessor.Controllers
                 HourInterval = new HourInterval(),
                 Jobs = _context.Jobs.ToList()
             };
-            return View("CreateMyJob", viewModel);
+            return View("CreateFormMyJobs", viewModel);
         }
 
         public ActionResult Edit(int id)
         {
-            var hourInterval = _context.HourIntervals.Include(i => i.Job).SingleOrDefault(i => i.Id == id);
+            var currentUserId = GetCurrentUserId();
+            var hourInterval = _context.HourIntervals.Include(h => h.Job).SingleOrDefault(h => h.Id == id && h.ApplicationUserId == currentUserId);
             if (hourInterval == null)
                 return HttpNotFound();
 
@@ -61,16 +63,33 @@ namespace TaskAssessor.Controllers
                 Jobs = _context.Jobs.ToList()
             };
 
-            return View("CreateMyJob", hourIntervalJobs);
+            return View("CreateFormMyJobs", hourIntervalJobs);
         }
+
         [HttpPost]
-        public ActionResult CreateMyJob(HourInterval hourInterval)
+        public ActionResult CreateFormMyJobs(HourInterval hourInterval)
         {
+            /*Getting acces to validation data with ModelState property
+            if ModelState is not valid
+                we return the same view, the view that include the customer form
+            else
+                we save or update
+             */
+            if (!ModelState.IsValid)
+            {
+
+                var viewModel = new HourIntervalJobs
+                {
+                    HourInterval = hourInterval,
+                    Jobs = _context.Jobs.ToList()
+                };
+                return View("CreateFormMyJobs",viewModel);
+            }
+
             if (hourInterval.Id == 0)
             {
                 hourInterval.ApplicationUserId = User.Identity.GetUserId();
                 hourInterval.DateAdded = DateTime.Now;
-                hourInterval.TotalTime = Math.Round((hourInterval.TimeEnded - hourInterval.TimeStarted).TotalHours, 2);
                 _context.HourIntervals.Add(hourInterval);
             }else
             {
@@ -82,13 +101,12 @@ namespace TaskAssessor.Controllers
                 hourIntervalInDb.ApplicationUserId = hourInterval.ApplicationUserId;
                 hourIntervalInDb.JobId = hourInterval.JobId;
                 hourIntervalInDb.DateAdded = hourIntervalInDb.DateAdded;
-                hourIntervalInDb.TotalTime = Math.Round((hourInterval.TimeEnded - hourInterval.TimeStarted).TotalHours, 2);
+                hourIntervalInDb.TotalTime = hourInterval.TotalTime;
             }
             _context.SaveChanges();
             return RedirectToAction("Index", "MyJobs");
 
         }
-
 
 
         /************************************************************************************************/
@@ -97,6 +115,10 @@ namespace TaskAssessor.Controllers
             _context.Dispose();
         }
 
+        private string GetCurrentUserId()
+        {
+            return User.Identity.GetUserId();
+        }
         private string SetDate(DateTime date)
         {
             return date.ToShortDateString();
--- TaskAssessor/Models/HourInterval.cs
+++ TaskAssessor/Models/HourInterval.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Web;
 
@@ -8,11 +9,24 @@ namespace TaskAssessor.Models
     public class HourInterval
     {
         public int Id { get; set; }
+
+        [Display(Name = "Started at")]
         public TimeSpan TimeStarted { get; set; }
+
+        //will need here custom validation attribute. TimeEnded > TimeStarted
+        [Display(Name = "Ended at")]
         public TimeSpan TimeEnded { get; set; }
+
         public DateTime DateAdded { get; set; }
+
         public string Description { get; set; }
-        public double TotalTime { get; set; }
+
+        private double totalTime = 0;
+
+        public double TotalTime {
+            get { return totalTime; }
+            set { totalTime = Math.Round((TimeEnded - TimeStarted).TotalHours, 2); }
+        }
 
         public string ApplicationUserId { get; set; }
 
