2387433f55408065ba64cba4d242e41b9f7d7d8f
pcorneliu <corneliu@rubrikkgroup.com>
2018-01-25 15:57:02 +0200

    Edited some table fileds and another controller for Statistic

---
+++ TaskAssessor/Controllers/StatisticsController.cs
@@ -0,0 +1,54 @@
+﻿using Microsoft.AspNet.Identity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Principal;
+using System.Web;
+using System.Web.Mvc;
+
+namespace TaskAssessor.Controllers
+{
+    [Authorize]
+    public class StatisticsController : Controller
+    {
+        //instead of index
+        public ActionResult UserProfile()
+        {
+            ViewBag.CurrentUser = User.Identity.GetUserName() ;
+            return View();
+        }
+
+
+        //Stats per Task
+        public ActionResult HoursPerTask(string jobName)
+        {
+            return View();
+        }
+
+        public ActionResult GeneralStatsPerTask()
+        {
+            return View();
+        }
+
+
+        //Stats per User
+        public ActionResult HoursPerUser(string userName)
+        {
+            return View();
+        }
+
+        public ActionResult GeneralStatsPerUser()
+        {
+            return View();
+        }
+
+
+        //Combined
+        public ActionResult HoursPerUserTask()
+        {
+            return View();
+        }
+
+
+    }
+}
